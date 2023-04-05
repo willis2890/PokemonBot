04cf33bff3a9f458749939aa1263c27a031ba479
willis2890 <58802564+willis2890@users.noreply.github.com>
2023-04-05 15:07:38 -0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,30 @@
+#constants.py
+
+channel_id= "1092001464278331443"
+token = "MjE0NDk5NTQ1Mzk1MTAxNjk3.GIsQuc.JiZzW39AhCU7K0wxPLP5kmpo-9fLdx-Tay7pA4"
+
+positions = [
+    (-1499, 1135), #pb
+    (-1433, 1132), #gb
+    (-1366, 1135), #ub
+    (-1297, 1126), #pb
+    (-1211, 1126),  #mb
+    (-497,  1245) #gb for fishing
+]
+
+xValue = 440
+yValue = 1366
+caught = 0
+failed = 0
+ran = 0
+counter = 0
+fishOffSet = 1
+
+ball_map = {
+    'Common': 0,
+    'Uncommon': 0,
+    'Rare': 1,
+    'Super': 2,
+    'Legendary': 4,
+    'Shiny': 4
+}
